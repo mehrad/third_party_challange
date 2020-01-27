fb43d9564a2ab3b6df8110a6d57f018d23b73f31
Mahrad Ataeefard <m.ataeefard@takhfifan.com>
2020-01-27 13:59:10 +0330

    add more tests

--- lib/proxy.rb
+++ lib/proxy.rb
@@ -1,12 +1,14 @@
+require 'error'
+
 module Proxy
   def self.resize(options)
-    call_api(method: 'resize', options: options)  
+    api_call(method: 'resize', options: options)  
   end
 
   private
 
   # This part intentionally left unrefactored
-  def self.call_api(method:, options:)
+  def self.api_call(method:, options:)
     if method == 'resize'
       raise 'one of the url or file should exists' if options[:url].nil? && options[:file].nil? 
       raise 'widith and height should present on resize method' if options[:width].nil? | options[:height].nil?
--- spec/proxy_spec.rb
+++ spec/proxy_spec.rb
@@ -1,11 +1,49 @@
 require 'proxy'
+require 'error'
 
-RSpec.describe Proxy, "#api" do
-  context "#resize" do
-    it "return ok on valid options" do
-      expect(Proxy.resize({ width: 12123, height: 1232, url: 123 })).to  eq(:ok)
+RSpec.describe Proxy, '#api' do
+  context '#resize' do
+    it 'returns ok on valid options' do
+      expect(Proxy.resize(width: 2123, height: 1232, url: 123)).to  eq(:ok)
+    end
+
+    it 'raise error without url and file' do
+      expect { Proxy.resize(width: 2123, height: 1232) }.to raise_error(InCompleteOptionError)
+    end
+
+    it 'raise error without height' do
+      expect { Proxy.resize(width: 1232, url: 123) }.to raise_error(InCompleteOptionError)
+    end
+
+    it 'raise error without width' do
+      expect { Proxy.resize(height: 1232, url: 123) }.to raise_error(InCompleteOptionError)
+    end
+
+    it 'raise error without width and height' do
+      expect { Proxy.resize(url: 123) }.to raise_error(InCompleteOptionError)
+    end
+
+  end
+
+  context '#crop' do
+    it 'returns ok on valid options' do
+      expect(Proxy.crop(width: 2123, height: 1232, url: 123)).to  eq(:ok)
+    end
+
+    it 'raise error without url and file' do
+      expect { Proxy.crop(width: 2123, height: 1232) }.to raise_error(InCompleteOptionError)
+    end
+
+    it 'raise error without width and height' do
+      expect { Proxy.crop(width: 1232, url: 123) }.to raise_error(InCompleteOptionError)
+    end
+
+    it 'returns ok only with height' do
+      expect { Proxy.crop(height: 1232, url: 123) }.to raise_error(InCompleteOptionError)
+    end
+
+    it 'returns ok only with width' do
+      expect { Proxy.crop(width: 2123, url:1321)}
     end
   end
 end
-
-
